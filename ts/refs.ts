dcb6cd8fc9bd4b1a4f002f9efc22d0643e7aee03
John Walker <john.walker.dev@gmail.com>
2015-09-21 04:39:51 -0600

    feat(wtc):

---
+++ ts/refs.ts
@@ -0,0 +1,16 @@
+//grunt-start
+/// <reference path="bindings.ts" />
+/// <reference path="bootstrap.ts" />
+/// <reference path="directives.ts" />
+/// <reference path="index.ts" />
+/// <reference path="inject.ts" />
+/// <reference path="lib.d.ts" />
+/// <reference path="services.ts" />
+/// <reference path="state.ts" />
+/// <reference path="typings/angular-ui-router/angular-ui-router.d.ts" />
+/// <reference path="typings/angularjs/angular.d.ts" />
+/// <reference path="typings/jquery/jquery.d.ts" />
+/// <reference path="typings/lodash/lodash.d.ts" />
+/// <reference path="typings/ng-right/ng-right.d.ts" />
+/// <reference path="utils.ts" />
+//grunt-end
