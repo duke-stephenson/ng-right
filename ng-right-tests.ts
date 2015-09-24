905c9e146a263f49282ab264a97f63f3b156cc52
John Walker <john.walker.dev@gmail.com>
2015-09-23 19:28:51 -0600

    feat(view): added view, please work

--- ng-right-tests.ts
+++ ng-right-tests.ts
@@ -3,12 +3,82 @@
  * @version 9/21/15 4:33 PM
  */
 
-import {Ambient} from 'ng-right';
 
-@Ambient
-class TestAmbient {
+/// <reference path="./typings/tsd.d.ts" />
+
+
+import {Attribute, Ambient, Bootstrap, Controller} from 'ng-right';
+import {Inject, Component, Service, State, View} from 'ng-right';
+import {bindTwoWay, bindOneWay, bindString, bindExpression} from 'ng-right';
+
+
+let app = angular.module('app', ['ng']);
+
+
+
+@Component({
+    selector: 'web-app'
+})
+@View({
+    template: '<content></content>'
+})
+@Bootstrap({
+    module: app
+})
+class WebApp {
+
+    constructor() {
+        console.log('this', this);
+    }
+}
+
+var a = new WebApp();
+
+@State({
+    name: 'name',
+    templateUrl: 'a.tpl.html'
+})
+@Inject('One', 'Two')
+class StateTest {
+
+    @bindTwoWay two: {};
+    @bindOneWay one: string;
+    @bindString str: string;
+    @bindExpression exp: Function;
+}
+
+var state = new StateTest();
+
+
+@Attribute({
+    selector: 'attr'
+})
+class AttrTest {
 
 }
 
-var a;
-a = new TestAmbient();
+@Service({
+    serviceName: 'name'
+})
+class ServiceTest {
+
+}
+
+@Controller({
+    controllerName: 'name'
+})
+class CtrlName {
+
+}
+
+@Ambient({
+
+})
+class Amb {
+
+}
+
+new AttrTest();
+new ServiceTest();
+new CtrlName();
+new Amb();
