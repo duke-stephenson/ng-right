b26009bb7d2ee029876278501ea7a7747da095fd
John Walker <john.walker.dev@gmail.com>
2015-10-10 19:10:52 -0600

    feat(dts): finish auto defintion build

--- gulpfile.babel.js/index.js
+++ gulpfile.babel.js/index.js
@@ -9,43 +9,53 @@ import changeLog from 'gulp-conventional-changelog';
 import ts from 'gulp-typescript';
 import dts from 'dts-bundle';
 import del from 'del';
+import merge from 'merge2';
 
 
 gulp.task('changelog', () => {
-    return gulp.src('changelog.md')
-        .pipe(changeLog({
-            preset: 'angular'
-        }))
-        .pipe(gulp.dest('./'));
+  return gulp.src('changelog.md')
+    .pipe(changeLog({
+      preset: 'angular'
+    }))
+    .pipe(gulp.dest('./'));
 });
 
 gulp.task('rm', done => {
-    return del('lib', done);
+  return del('lib', done);
 });
 
-let project = ts.createProject('tsconfig.json');
+let project = ts.createProject('tsconfig.json', {
+  noExternalResolve: true,
+  failOnTypeErrors: true,
+  declaration: true
+});
 
 gulp.task('ts', () => {
-    return project.src()
-    .pipe(ts(project))
-    .js
-    .pipe(gulp.dest('lib'));
+
+  let result = project.src({base: './ts'})
+    .pipe(ts(project));
+
+  return merge([
+    result.js.pipe(gulp.dest('lib')),
+    result.dts.pipe(gulp.dest('lib'))
+  ]);
 });
 
 
-gulp.task('dts', () => {
+gulp.task('dts', (done) => {
 
-    return dts.bundle({
-        //externals: true,
-        name: 'ng-right',
-        main: 'lib/index.d.ts'
-    });
+  dts.bundle({
+    //externals: true,
+    name: 'ng-right',
+    main: 'lib/ts/index.d.ts',
+    out: 'index.d.ts'
+  });
+  return done();
 
 });
 
 gulp.task('run', () => {
-    gulp.watch('ts/**/*.ts', gulp.series('ts'));
+  gulp.watch('ts/**/*.ts', gulp.series('ts'));
 });
 
 gulp.task('default', gulp.series('rm', 'ts', 'dts'));
-
